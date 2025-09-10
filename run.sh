646e03cc11fe4b0fe01e40d5b20d9dc7e8026408
Ptozin <up202004293@up.pt>
2025-09-10 23:40:09 +0100

    .

--- run.sh
+++ run.sh
@@ -127,7 +127,9 @@ if [[ "${SKIPUPDATE,,}" != "true" ]]; then
         printf "\\nRemoving the app manifest to force Steam to check for an update...\\n"
         rm "/config/gamefiles/steamapps/appmanifest_1690800.acf" || true
     fi
-    steamcmd +force_install_dir /config/gamefiles +login anonymous +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate +quit
+    # -manifest <MANIFEST_ID>
+    # steamcmd +force_install_dir /config/gamefiles +login anonymous +app_update 1690800 -beta public -validate -manifest 30903977 +quit
+    steamcmd +force_install_dir /config/gamefiles +login anonymous +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate -manifest 30903977 +quit
     cp -r /home/steam/.steam/steam/logs/* "/config/logs/steam" || printf "Failed to store Steam logs\\n"
 else
     printf "Skipping update as flag is set\\n"
